194b299aed1057e7b79b46454eecf3533665e9c3
Alceu-Pereira <alceupereira3105@gmail.com>
2024-06-04 22:55:58 -0300

    day-6-4-24

---
+++ secao_5/aula120_introducao_a_classes.py
@@ -0,0 +1,19 @@
+# string = 'Alceu'
+# print(string.upper())
+# print(isinstance(string, str))
+
+
+class Pessoa:
+    pass
+
+pessoa_1 = Pessoa()
+pessoa_1.nome = 'Alceu'
+pessoa_1.sobrenome = 'Pereira'
+print(pessoa_1.nome)
+print(pessoa_1.sobrenome)
+
+pessoa_2 = Pessoa()
+pessoa_2.nome = 'Fulano'
+pessoa_2.sobrenome = 'Beltrano'
+print(pessoa_2.nome)
+print(pessoa_2.sobrenome)
