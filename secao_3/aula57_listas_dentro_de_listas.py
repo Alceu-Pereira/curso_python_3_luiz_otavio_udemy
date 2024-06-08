863bf9fb7535d5d7d5504e097ea83e3064acc080
Alceu-Pereira <alceupereira3105@gmail.com>
2024-06-08 14:55:53 -0300

    day-6-8-24

---
+++ secao_3/aula57_listas_dentro_de_listas.py
@@ -0,0 +1,9 @@
+classrooms = [
+    ['Mary', 'Helena',],
+    ['Elaine',],
+    ['Louis', 'John', 'Eduard'],
+]
+
+for classroom in classrooms:
+    for students in classroom:
+        print(students)
