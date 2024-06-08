f98c61418c52d1b9d30b7a41aec44341ccdcc69f
Alceu-Pereira <alceupereira3105@gmail.com>
2024-06-08 17:37:54 -0300

    day-6-8-24

---
+++ secao_3/aula60_operador_condicional_ternario.py
@@ -0,0 +1,10 @@
+# name = 'Alceu'
+
+# male = True
+# female = False
+
+# print(f'{name} é masculino {male if male else female}')
+
+number = 10
+higher_than_9 = number if number > 10 else 0
+print(higher_than_9)
