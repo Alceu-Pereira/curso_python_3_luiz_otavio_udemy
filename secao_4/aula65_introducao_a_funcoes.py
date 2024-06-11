8d79883fdff178673d0d967e7d6e07ca1cf48d30
Alceu-Pereira <alceupereira3105@gmail.com>
2024-06-10 21:45:35 -0300

    day-6-10-24

---
+++ secao_4/aula65_introducao_a_funcoes.py
@@ -0,0 +1,11 @@
+# def Print(a, b, c):
+#     print(a)
+#     print(b)
+#     print(c)
+
+# Print('a', 'b', 'c')
+def saudacao(nome='Sem nome'):
+    print(f'Olá {nome}!')
+
+saudacao('Alceu')
+saudacao()
