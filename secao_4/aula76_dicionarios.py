9f4ba9dccecd32348741dca1288234d74508aaef
Alceu-Pereira <alceupereira3105@gmail.com>
2024-06-13 21:17:57 -0300

    aula76_dicionarios

---
+++ secao_4/aula76_dicionarios.py
@@ -0,0 +1,11 @@
+pessoa = {
+    'nome': 'Alceu',
+    'sobrenome': 'Pereira',
+}
+
+pessoa2 = dict(nome='Alceu', sobrenome='Pereira')
+
+print(pessoa['sobrenome'])
+
+for k in pessoa:
+    print(pessoa[k])
