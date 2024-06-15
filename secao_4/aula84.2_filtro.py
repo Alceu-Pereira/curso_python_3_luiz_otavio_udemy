e68f57b55e70710a6a290845d99a254b7bc16809
Alceu-Pereira <alceupereira3105@gmail.com>
2024-06-14 22:21:17 -0300

    aula84.2_filtro

---
+++ secao_4/aula84.2_filtro.py
@@ -0,0 +1,28 @@
+# print(list(range(10)))
+
+# lista = []
+# for i in range(10):
+#     lista.append(i)
+# print(lista)
+
+# lista = [
+#     i ** 2
+#     for i in range(10)
+# ]
+# print(lista)
+
+produtos = [
+    {'nome': 'Produto 1', 'preco': 20, },
+    {'nome': 'Produto 2', 'preco': 10, },
+    {'nome': 'Produto 3', 'preco': 30, },
+]
+
+novos_produtos = [
+    {**produto, 'preco': produto['preco'] * 1.05}
+    if produto['preco'] == 10 
+    else {**produto}
+    for produto in produtos
+    if produto['preco'] < 30
+]
+
+print(*novos_produtos, sep='\n')
