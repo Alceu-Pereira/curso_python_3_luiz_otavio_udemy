cca310435ae6492a6b8f2a49bbec4b0e7e72624b
Alceu-Pereira <alceupereira3105@gmail.com>
2024-06-16 21:22:48 -0300

    aula93.1_excecoes

---
+++ secao_4/aula93.1_excecoes.py
@@ -0,0 +1,13 @@
+# a = 18
+b = 'k'
+
+try:
+    c = a / b
+except ZeroDivisionError:
+    print('Dividiu por zero.')
+
+except NameError as name_error:
+    print(f'Falta definir a variável {name_error.name}')
+
+except Exception:
+    print('Erro desconhecido')
