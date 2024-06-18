45dcab26b2ccb82978fab456e723e791ac7711f9
Alceu-Pereira <alceupereira3105@gmail.com>
2024-06-18 00:19:06 -0300

    aula130_method_classmethod_staticmethod

---
+++ secao_5/aula130_method_classmethod_staticmethod.py
@@ -0,0 +1,36 @@
+class Connection:
+    def __init__(self, host='localhost'):
+        self.host = host
+        self.user = None
+        self.password = None
+
+    def set_user(self, user):
+        self.user = user
+
+    def set_password(self, password):
+        self.password = password
+
+    @classmethod
+    def default_user(cls, user='admin', password='admin'):
+       cls.user = user
+       cls.password = password
+       return cls
+
+    @staticmethod
+    def log(msg):
+        print('log:', msg)
+
+c1 = Connection()
+c1.user = 'Alceu'
+c1.password = 123
+print(c1.user)
+print(c1.password)
+
+
+
+c2 = Connection.default_user()
+print(c2.user)
+print(c2.password)
+
+
+Connection.log('Log')
