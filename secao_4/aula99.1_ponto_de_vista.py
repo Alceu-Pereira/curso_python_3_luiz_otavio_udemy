d412429f1d6ddd1b79de6c0ebc8726420330572c
Alceu-Pereira <alceupereira3105@gmail.com>
2024-06-18 19:35:22 -0300

    aula99.1_ponto_de_vista

---
+++ secao_4/aula99.1_ponto_de_vista.py
@@ -0,0 +1,14 @@
+# from sys import path
+# # from aula99_pacote import modulo
+# # import aula99_pacote.modulo
+# from aula99_pacote.modulo import *
+
+# # print(*path, sep='\n')
+# # print(aula99_pacote.modulo.soma(2, 6))
+
+# print(soma(2, 5))
+# print(variavel)
+# print(nova_variavel)
+from aula99_1_pacote.modulo import soma
+
+print(__name__)
