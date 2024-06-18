c153f1ad73ac5ffc769e826fbeeda791e0299c5c
Alceu-Pereira <alceupereira3105@gmail.com>
2024-06-18 13:15:08 -0300

    aula99_pacotes

---
+++ secao_4/aula99_pacote/modulo.py
@@ -0,0 +1,8 @@
+__all__ = ['variavel', 'soma', 'nova_variavel']
+
+variavel = 'Algo'
+def soma(x, y):
+
+    return x + y
+
+nova_variavel = 'Outra'
---
+++ secao_4/aula99_pacotes.py
@@ -0,0 +1,11 @@
+from sys import path
+# from aula99_pacote import modulo
+# import aula99_pacote.modulo
+from aula99_pacote.modulo import *
+
+# print(*path, sep='\n')
+# print(aula99_pacote.modulo.soma(2, 6))
+
+print(soma(2, 5))
+print(variavel)
+print(nova_variavel)
