d37bd6a42ee82028b831aad0fbf10088bc31cb91
Alceu-Pereira <alceupereira3105@gmail.com>
2024-06-17 21:52:32 -0300

    aula96_import_from_as_all

---
+++ secao_4/aula96_import_from_as_all.py
@@ -0,0 +1,20 @@
+# import sys
+
+# print(sys.platform)
+
+# from sys import exit, platform
+
+# exit()
+# print(platform)
+
+# import sys as my_sys
+
+# print(my_sys.platform)
+
+# from sys import platform as pf, exit as ex
+# ex()
+# print(pf)
+
+from sys import *
+
+print(platform)
