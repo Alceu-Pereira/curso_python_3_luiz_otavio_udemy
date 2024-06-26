7fc2448d988ac509f86f286c8e90cedecb749009
Alceu-Pereira <alceupereira3105@gmail.com>
2024-06-26 13:04:26 -0300

    aula111_partial_e_map

---
+++ secao_4/aula111_partial_e_map.py
@@ -0,0 +1,35 @@
+from functools import partial
+
+def print_iter(iterator):
+    print(*list(iterator), sep='\n')
+    print()
+
+produtos = [
+    {'nome': 'Produto 5', 'preco': 10.00},
+    {'nome': 'Produto 1', 'preco': 22.32},
+    {'nome': 'Produto 3', 'preco': 10.11},
+    {'nome': 'Produto 2', 'preco': 105.87},
+    {'nome': 'Produto 4', 'preco': 69.90},
+]
+
+def aumentar_porcentagem(valor, porcentagem):
+    return round(valor * porcentagem, 2)
+
+
+aumentar_dez_por_cento = partial(aumentar_porcentagem, porcentagem=1.1)
+
+
+# novos_produtos = [
+
+#     {**p, 'preco': aumentar_dez_por_cento(p['preco'])} for p in produtos
+
+# ]
+
+def muda_preco(produto):
+    return {**produto, 'preco': aumentar_dez_por_cento(produto['preco'])}
+
+
+novos_produtos = map(muda_preco, produtos)
+print_iter(produtos)
+print_iter(novos_produtos)
+print(type(novos_produtos))
