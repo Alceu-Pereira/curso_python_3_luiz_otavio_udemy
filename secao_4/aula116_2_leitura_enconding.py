49ffa96b7007198f8cfab21dd93a73eb3a6412af
Alceu-Pereira <alceupereira3105@gmail.com>
2024-06-27 21:05:55 -0300

    aula116_2_leitura_enconding

---
+++ secao_4/aula116_2_leitura_enconding.py
@@ -0,0 +1,13 @@
+caminho_arquivo = 'C:\\Users\\AlceuP\\Desktop\\Cursos\\curso_python_3_luiz_otavio_udemy\\secao_4\\'
+caminho_arquivo +=  'aula116_2_leitura_enconding.txt'
+
+with open(caminho_arquivo, 'w+', encoding='UTF-8') as arquivo:
+    arquivo.write('Linha 1\n')
+    arquivo.write('Linha 2\n')
+    arquivo.write('Ãáà\n')
+    arquivo.writelines(
+        [f'{str(n)}\n' for n in range(5)]
+    )
+    arquivo.seek(0, 0)
+    print(arquivo.read())
+    
