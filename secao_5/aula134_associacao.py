69623676746f849d2a555ae744692bd586000c6d
Alceu-Pereira <alceupereira3105@gmail.com>
2024-06-28 23:08:12 -0300

    aula134_associacao

---
+++ secao_5/aula134_associacao.py
@@ -0,0 +1,28 @@
+class Escritor:
+    def __init__(self, nome):
+        self.nome = nome
+        self._ferramenta = None
+        
+    @property
+    def ferramenta(self):
+        return self._ferramenta
+    
+    @ferramenta.setter
+    def ferramenta(self, ferramenta):
+        self._ferramenta = ferramenta
+
+
+class FerramentaDeEscrever:
+    def __init__(self, nome):
+        self.nome = nome
+
+    def escrever(self):
+        return f'{self.nome} está escrevendo'
+    
+
+escritor = Escritor('Alceu')
+caneta = FerramentaDeEscrever('Caneta')
+maquina = FerramentaDeEscrever('Máquina')
+escritor.ferramenta = maquina
+
+print(escritor.ferramenta.escrever())
